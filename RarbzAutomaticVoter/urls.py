81aba93703e1a4a9ea167cd36ab8cc0345b96ea4
Dininta Isnurthina <dini.dininta@gmail.com>
2021-10-22 15:26:30 +0700

    Migrate database and change app url

--- RarbzAutomaticVoter/urls.py
+++ RarbzAutomaticVoter/urls.py
@@ -17,6 +17,6 @@ from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
-    url('rojevoter/', include('RarbzAutomaticVoterApp.urls')),
+    url('', include('RarbzAutomaticVoterApp.urls')),
     url(r'^admin/', admin.site.urls),
 ]
