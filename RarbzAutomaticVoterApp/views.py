34f12134c3e4835fc49f73c05cf81b9844c19203
Dininta Isnurthina <dini.dininta@gmail.com>
2021-10-22 23:02:00 +0700

    Set up selenium webdriver and webdriver manager for Chrome

--- RarbzAutomaticVoterApp/views.py
+++ RarbzAutomaticVoterApp/views.py
@@ -1,5 +1,10 @@
 from django.http import HttpResponse
-
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+import time
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+  driver = webdriver.Chrome(ChromeDriverManager().install())
+  driver.get("https://www.mtvema.com/vote/bestk-pop/")
+  time.sleep(300)
+  return HttpResponse("Hello, world. You're at the polls index.")
